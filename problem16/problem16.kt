aabc6794232ffc5aeaead8f8e585e58743e0fd4a
Nathan Gilbert <nathan.gilbert@gmail.com>
2018-02-19 19:56:35 -0700

    some functioning code

--- problem16/problem16.kt
+++ problem16/problem16.kt
@@ -1,7 +1,18 @@
 /*
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-What is the sum of the digits of the number 21000?
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2^1000?
 */
-fun main(args: Array<String>) {
+public fun main(args: Array<String>) {
+    val base = 2
+    val exp = 15
+    val result = Math.pow(base.toDouble(), exp.toDouble())
+    println("$base^$exp = $result")
 
+    //convert result to stringa
+    val resultStr = result.toInt().toString()
+    var sum = 0
+    for (ch in resultStr) {
+        sum += ch.toInt()
+    }
+    println("The sum is $sum")
 }
