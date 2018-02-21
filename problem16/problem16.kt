4bc0713245fcbc2aab8e5fb33a1d3c9587aecbb4
Nathan Gilbert <nathan.gilbert@gmail.com>
2018-02-20 17:40:59 -0700

    fixed it

--- problem16/problem16.kt
+++ problem16/problem16.kt
@@ -4,34 +4,11 @@ What is the sum of the digits of the number 2^1000?
 */
 import java.math.BigInteger
 
-fun sumOfDigits(n: BigInteger) {
-    var sum = BigInteger.ZERO;
-    while (n.compareTo(BigInteger.ZERO) == 1) {
-        sum = sum.add(n.remainder(ten));
-        n = n.divide(ten);
-    }
-    return sum;
-}
-
 fun main(args: Array<String>) {
-    val base = 2
-    //val exp = 15
-    val exp = 1000
-    val result = Math.pow(base.toDouble(), exp.toDouble())
-    println("$base^$exp = $result")
-
-    //convert result to stringa
-    var resultBig:BigInteger = result.toBigInteger()
-    /*
-    val resultStr = result.toBigInteger().toString()
-    println(resultStr)
+    val a = BigInteger("2").pow(1000)
     var sum = 0
-    for (ch in resultStr) {
-        //have to convert chars to string before going to int
+    for (ch in a.toString()) {
         sum += ch.toString().toInt()
     }
-    */
-
-    val sum = sumOfDigits(resultBig)
     println("The sum: $sum")
 }
