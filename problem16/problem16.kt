48a5c0912790f9a31d348756b62ca3d1b843f4ac
Nathan Gilbert <nathan.gilbert@gmail.com>
2018-02-20 17:29:35 -0700

    going biginteger

--- problem16/problem16.kt
+++ problem16/problem16.kt
@@ -2,7 +2,18 @@
 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 What is the sum of the digits of the number 2^1000?
 */
-public fun main(args: Array<String>) {
+import java.math.BigInteger
+
+fun sumOfDigits(n: BigInteger) {
+    var sum = BigInteger.ZERO;
+    while (n.compareTo(BigInteger.ZERO) == 1) {
+        sum = sum.add(n.remainder(ten));
+        n = n.divide(ten);
+    }
+    return sum;
+}
+
+fun main(args: Array<String>) {
     val base = 2
     //val exp = 15
     val exp = 1000
@@ -10,12 +21,17 @@ public fun main(args: Array<String>) {
     println("$base^$exp = $result")
 
     //convert result to stringa
-    val resultStr = result.toLong().toString()
+    var resultBig:BigInteger = result.toBigInteger()
+    /*
+    val resultStr = result.toBigInteger().toString()
     println(resultStr)
     var sum = 0
     for (ch in resultStr) {
         //have to convert chars to string before going to int
         sum += ch.toString().toInt()
     }
+    */
+
+    val sum = sumOfDigits(resultBig)
     println("The sum: $sum")
 }
